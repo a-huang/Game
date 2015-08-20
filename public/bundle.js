5b164ebc099d95946673c80fe9dc2fc0e0399755
Alex Huang <huang.alexh@gmail.com>
2015-08-20 17:17:19 -0500

    Added line to log

--- public/bundle.js
+++ public/bundle.js
@@ -28342,16 +28342,39 @@ module.exports = shaders;
 'use strict';
 
 var DOMElement = require('famous/dom-renderables/DOMElement');
+var Position = require('famous/components/Position');
 
 function Log(node) {
-	this.node = node;
-	this.el = new DOMElement(node);
-	this.el.setProperty('backgroundImage', 'url(images/log.png)');
+  this.node = node;
+  this.node.setSizeMode(1, 1, 1).setAbsoluteSize(50, 200).setPosition(window.innerWidth / 2, window.innerHeight / 2, 0).setMountPoint(0.5, 1);
+
+  this.el = new DOMElement(node);
+  this.el.setProperty('backgroundImage', 'url(images/log.png)');
+
+  //Create target line
+  this.line = this.node.addChild();
+  this.line.setAbsoluteSize(50, 6).setSizeMode(1, 1, 1).setAlign(0.0, randomAlign.call() / 8); //need to make function to radomize this
+  var targetLine = new DOMElement(this.line, {
+    properties: {
+      'background-color': '#FF0000'
+    },
+    content: ' '
+  });
+
+  this.position = new Position(this.node);
+  this.position.set(window.innerWidth / 2, 0, 0, { duration: 5000 });
+}
+
+// randomAlign() produces a number between 1 and 7
+function randomAlign() {
+  var rand = Math.ceil(Math.random() * 7);
+  console.log(rand);
+  return rand;
 }
 
 module.exports = Log;
 
-},{"famous/dom-renderables/DOMElement":28}],142:[function(require,module,exports){
+},{"famous/components/Position":7,"famous/dom-renderables/DOMElement":28}],142:[function(require,module,exports){
 'use strict';
 
 var Log = require('./Log.js');
@@ -28363,7 +28386,7 @@ var FamousEngine = require('famous/core/FamousEngine');
 FamousEngine.init();
 
 var game = FamousEngine.createScene('body').addChild();
-var log = new Log(game.addChild());
-log.node.setSizeMode(1, 1, 1).setAbsoluteSize(50, 200).setPosition(window.innerWidth / 2, window.innerHeight / 2, 0).setMountPoint(0.5, 1);
+var log = new Log(game.addChild()); //log test
+log.node;
 
 },{"./Log.js":141,"famous":46,"famous/core/FamousEngine":18,"famous/dom-renderables/DOMElement":28}]},{},[142]);
--- src/index.js
+++ src/index.js
@@ -7,8 +7,4 @@ var FamousEngine = require('famous/core/FamousEngine');
  FamousEngine.init();
 
 var game = FamousEngine.createScene('body').addChild();
-var log = new Log(game.addChild()); //log test 
-	log.node.setSizeMode(1,1,1)
-        .setAbsoluteSize(50, 200)
-        .setPosition(window.innerWidth / 2, window.innerHeight / 2, 0)
-        .setMountPoint(0.5, 1); 
+var log = new Log(game.addChild());
